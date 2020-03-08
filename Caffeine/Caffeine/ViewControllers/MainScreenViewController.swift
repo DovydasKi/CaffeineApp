421f1f056c0a96d7d075c49871c30f15f7543de7
Dovydas Kiriliauskas <dovydasdk@gmail.com>
2020-03-08 17:47:48 +0200

    Constraints

--- Caffeine/Caffeine/ViewControllers/MainScreenViewController.swift
+++ Caffeine/Caffeine/ViewControllers/MainScreenViewController.swift
@@ -49,7 +49,7 @@ extension MainScreenViewController {
     private func initCaffeineLogo() -> UIImageView {
         let image = UIImage(named: "bolt.png")
         let imageView = UIImageView(image: image!)
-        imageView.frame = CGRect(x: 0, y: 0, width: 97, height: 80)
+        imageView.frame = CGRect(x: 0, y: 0, width: 120, height: 120)
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
         return imageView
@@ -57,22 +57,24 @@ extension MainScreenViewController {
 
     private func initCaffeineLabel(_ title: String) -> UILabel {
         let label = UILabel()
-        label.font = UIFont(name: "Rubik-Bold", size: 54)
+		label.font = UIFont(name: "Rubik-Bold", size: 54)
         label.text = title
         label.textColor = .white
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textAlignment = .center
+		label.contentMode = .scaleAspectFit
         return label
     }
 
     private func initLoginInButton(_ title: String) -> UIButton {
-        let button: UIButton = UIButton(frame: CGRect(x: 56, y: 438, width: 304, height: 69))
+        let button: UIButton = UIButton()
         button.layer.cornerRadius = 34.5
         button.layer.shadowColor = UIColor.black.cgColor
         button.layer.shadowOffset = CGSize(width: 0, height: 2)
         button.layer.shadowRadius = 4 // blur
         button.layer.shadowOpacity = 0.5
         button.backgroundColor = .white
+		button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle(title, for: .normal)
         button.setTitleColor(UIColor(named: "orangeMain"), for: .normal)
         button.titleLabel?.font = UIFont(name: "Rubik-Bold", size: 32)
@@ -81,13 +83,14 @@ extension MainScreenViewController {
     }
 
     private func initGetStartedButton(_ title: String) -> UIButton {
-        let button: UIButton = UIButton(frame: CGRect(x: 56, y: 539, width: 304, height: 69))
+		let button: UIButton = UIButton()
         button.layer.cornerRadius = 34.5
         button.layer.shadowColor = UIColor.black.cgColor
         button.layer.shadowOffset = CGSize(width: 0, height: 2)
-        button.layer.shadowRadius = 4 // blur
+        button.layer.shadowRadius = 4
         button.layer.shadowOpacity = 0.5
         button.backgroundColor = .white
+		button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle(title, for: .normal)
         button.setTitleColor(UIColor(named: "orangeMain"), for: .normal)
         button.titleLabel?.font = UIFont(name: "Rubik-Bold", size: 32)
@@ -99,31 +102,41 @@ extension MainScreenViewController {
 extension MainScreenViewController {
     private func setCaffeineLogoConstraints(_ caffeineLogo: UIImageView) {
         NSLayoutConstraint.activate([
-            self.caffeineLogo.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 91.5),
-            self.caffeineLogo.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 158.5),
-            self.caffeineLogo.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -158.5),
-            self.caffeineLogo.heightAnchor.constraint(equalToConstant: 165)])
+				self.caffeineLogo.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+				self.caffeineLogo.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: UIView.margin(of: [48.5, 72.75, 97.0])),
+				self.caffeineLogo.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: UIView.margin(of: [42.0, 63.0, 84.0])),
+				self.caffeineLogo.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -UIView.margin(of: [42.0, 63.0, 84.0])),
+				self.caffeineLogo.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: UIView.margin(of: [100, 220.0, 300.0]))
+		])
     }
 
     private func setCaffeineLabelConstraints(_ caffeineLabel: UILabel) {
         NSLayoutConstraint.activate([
-            self.caffeineLabel.topAnchor.constraint(equalTo: self.caffeineLogo.bottomAnchor, constant: 26.5),
-            self.caffeineLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 58.5),
-            self.caffeineLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -58.5),
-            self.caffeineLabel.heightAnchor.constraint(equalToConstant: 65)])
+			self.caffeineLabel.topAnchor.constraint(equalTo: self.caffeineLogo.bottomAnchor, constant: UIView.margin(of: [7.5, 13.25, 26.5])),
+			self.caffeineLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: UIView.margin(of: [14.5, 29.25, 58.5])),
+			self.caffeineLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -UIView.margin(of: [14.5, 29.25, 58.5])),
+			self.caffeineLabel.bottomAnchor.constraint(equalTo: self.caffeineLogo.bottomAnchor, constant:  UIView.margin(of: [21, 55, 80.5]))
+		])
     }
 
     private func setLoginButtonConstraints(_ loginButton: UIButton) {
         NSLayoutConstraint.activate([
-            self.loginButton.topAnchor.constraint(equalTo: self.caffeineLabel.bottomAnchor, constant: 103),
-            self.loginButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 55),
-            self.loginButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -55)])
+			self.loginButton.heightAnchor.constraint(equalToConstant: 69),
+			self.loginButton.widthAnchor.constraint(equalToConstant: UIView.margin(of: [16, 152.0, 304.0])),
+			self.loginButton.topAnchor.constraint(equalTo: self.caffeineLabel.bottomAnchor, constant: UIView.margin(of: [16, 70, 103.0])),
+			self.loginButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: UIView.margin(of: [16, 30, 55.0])),
+            self.loginButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -UIView.margin(of: [16, 30, 55.0]))
+		])
     }
 
     private func setGetStartedButtonConstraints(_ getStartedButton: UIButton) {
         NSLayoutConstraint.activate([
-            self.getStartedButton.topAnchor.constraint(equalTo: self.caffeineLabel.bottomAnchor, constant: 232),
-            self.getStartedButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 55),
-            self.getStartedButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -55)])
+			self.getStartedButton.heightAnchor.constraint(equalToConstant: 69),
+			self.getStartedButton.widthAnchor.constraint(equalToConstant: UIView.margin(of: [16, 152.0, 304.0])),
+			self.getStartedButton.topAnchor.constraint(equalTo: self.loginButton.bottomAnchor, constant: UIView.margin(of: [15, 30, 60])),
+            self.getStartedButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: UIView.margin(of: [16, 30, 55.0])),
+            self.getStartedButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -UIView.margin(of: [16, 30, 55.0]))
+		])
     }
 }
+
