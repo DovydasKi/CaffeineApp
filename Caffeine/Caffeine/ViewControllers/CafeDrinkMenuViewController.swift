23a1b85903e4c97c6b6817e5f2c5727424812a43
danikkk <dldanown@gmail.com>
2020-03-21 19:20:55 +0200

    another implementation

--- Caffeine/Caffeine/ViewControllers/CafeDrinkMenuViewController.swift
+++ Caffeine/Caffeine/ViewControllers/CafeDrinkMenuViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 class Coffee {
     var imageView: UIImageView?
     var label: UILabel?
-    
+
     init(imageView: UIImageView, label: UILabel) {
         self.imageView = imageView
         self.label = label
@@ -20,46 +20,45 @@ class Coffee {
 }
 
 class CafeDrinkMenuViewController: UIViewController {
-    
+
     private lazy var searchBar: UISearchBar = self.initSearchBar()
-    
-    private lazy var imageViewArray: [UIImageView] = initImageViewArray()
-    private lazy var imageTitle: [UILabel] = initImageTitleArray()
-    
+    private lazy var titleLabel: UILabel = self.initTitleLabel()
     private lazy var coffeeArray: [Coffee] = initCoffeeArray()
-    private lazy var firstColumnCoffeeStackView: [UIStackView] = initColumnCoffeeStackView(sizeOfCoffeArray: coffeeArray.count, numberOfColumns: 2, isFirstColumn: true)
-    private lazy var secondColumnCoffeeStackView: [UIStackView] = initColumnCoffeeStackView(sizeOfCoffeArray: coffeeArray.count, numberOfColumns: 2, isFirstColumn: false)
+    private lazy var firstColumnCoffeeStackView: [UIStackView] = initNthColumnCoffeeStackView(coffeArray: self.coffeeArray, sizeOfCoffeArray: self.coffeeArray.count, numberOfColumns: 2, isFirstColumn: true)
+    private lazy var secondColumnCoffeeStackView: [UIStackView] = initNthColumnCoffeeStackView(coffeArray: self.coffeeArray, sizeOfCoffeArray: self.coffeeArray.count, numberOfColumns: 2, isFirstColumn: false)
     private lazy var firstColumnStackView: UIStackView = initFirstColumnStackView()
     private lazy var secondColumnStackView: UIStackView = initSecondColumnStackView()
     private lazy var imageStackView: UIStackView = initImageStackView()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.isHidden = true
         self.view.backgroundColor = .white
-        
+
         self.view.addSubview(self.searchBar)
         self.activateSearchBarConstraints()
         
-        
+        self.view.addSubview(titleLabel)
+        self.activateTitleLabelConstraints()
+
+
         for item in coffeeArray {
             self.view.addSubview(item.imageView!)
             self.view.addSubview(item.label!)
         }
-        
-        for item in firstColumnCoffeeStackView {
-            self.view.addSubview(item)
+
+        for firstStackViewItem in firstColumnCoffeeStackView {
+            self.view.addSubview(firstStackViewItem)
         }
-        
-        for item in secondColumnCoffeeStackView {
-            self.view.addSubview(item)
+
+        for secondStackViewItem in secondColumnCoffeeStackView {
+            self.view.addSubview(secondStackViewItem)
         }
-        
-        
+
         self.view.addSubview(self.imageStackView)
         self.setImageStackViewConstraints()
-        
-        
+
+
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
@@ -74,18 +73,29 @@ extension CafeDrinkMenuViewController {
         searchBar.searchTextField.attributedPlaceholder = NSAttributedString(string: "Ieškoti")
         searchBar[keyPath: \.searchTextField].font = UIFont(name: "Rubik-Medium", size: 17)
         searchBar[keyPath: \.searchTextField].textColor = UIColor(named: "mainOrange")
-        
+
         searchBar.translatesAutoresizingMaskIntoConstraints = false
         return searchBar
     }
     
+    private func initTitleLabel() -> UILabel {
+        let titleLabel = UILabel()
+        titleLabel.text = "CAFFEINE"
+        titleLabel.font = UIFont(name: "Rubik-Black", size: 32)
+        titleLabel.textColor = UIColor(named: "orangeMain")
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textAlignment = .left
+        titleLabel.contentMode = .scaleAspectFit
+        return titleLabel
+    }
+
     private func initCoffeeArray() -> [Coffee] {
         var coffeeArray = [Coffee]()
-        
+
         for _ in 1...6 {
             coffeeArray.append(Coffee(imageView: UIImageView(), label: UILabel()))
         }
-        
+
         coffeeArray[0].imageView?.image = #imageLiteral(resourceName: "cappuccino")
         coffeeArray[0].label?.text = "Cappuccino"
         coffeeArray[1].imageView?.image = #imageLiteral(resourceName: "latte")
@@ -98,93 +108,59 @@ extension CafeDrinkMenuViewController {
         coffeeArray[4].label?.text = "Mocha"
         coffeeArray[5].imageView?.image = #imageLiteral(resourceName: "flatWhite")
         coffeeArray[5].label?.text = "Flat White"
-        
+
         for item in coffeeArray {
-            item.imageView!.contentMode = .scaleAspectFill
-            item.imageView!.layer.masksToBounds = true
-            item.imageView!.translatesAutoresizingMaskIntoConstraints = false
-            item.imageView!.heightAnchor.constraint(equalToConstant: 116).isActive = true
-            item.imageView!.widthAnchor.constraint(equalToConstant: 174).isActive = true
+            item.imageView?.contentMode = .scaleAspectFill
+            item.imageView?.layer.masksToBounds = true
+            item.imageView?.layer.cornerRadius = 12.5
+            item.imageView?.layer.shadowColor = UIColor.black.cgColor
+            item.imageView?.layer.shadowOffset = CGSize(width: 0, height: 2)
+            item.imageView?.layer.shadowRadius = 4
+            item.imageView?.layer.shadowOpacity = 0.5
+            item.label?.font = UIFont(name: "Rubik-Medium", size: 22)
+            item.label?.textColor = UIColor(named: "orangeMain")
+            item.imageView?.translatesAutoresizingMaskIntoConstraints = false
+            item.imageView?.heightAnchor.constraint(equalToConstant: 116).isActive = true
+            item.imageView?.widthAnchor.constraint(equalToConstant: 174).isActive = true
         }
-        
+
         return coffeeArray
     }
-    
-    private func initColumnCoffeeStackView(sizeOfCoffeArray: Int, numberOfColumns: Int, isFirstColumn: Bool) -> [UIStackView] {
+
+    private func initNthColumnCoffeeStackView(coffeArray: [Coffee], sizeOfCoffeArray: Int, numberOfColumns: Int, isFirstColumn: Bool) -> [UIStackView] {
         var stackView = [UIStackView]()
-        
+
         for _ in 1...sizeOfCoffeArray / numberOfColumns {
             stackView.append(UIStackView())
         }
-        
-        if isFirstColumn {
-            
-        }
-        for stackView in stackView {
-            stackView.axis = NSLayoutConstraint.Axis.vertical
-            stackView.distribution = UIStackView.Distribution.equalSpacing
-            stackView.alignment = UIStackView.Alignment.center
-            stackView.spacing = 4
+
+        for stackViewItem in stackView {
+            stackViewItem.axis = NSLayoutConstraint.Axis.vertical
+            stackViewItem.distribution = UIStackView.Distribution.equalSpacing
+            stackViewItem.alignment = UIStackView.Alignment.center
+            stackViewItem.spacing = 4
             if isFirstColumn {
-                for item in self.coffeeArray {
-                    stackView.addArrangedSubview(item.imageView!)
-                    stackView.addArrangedSubview(item.label!)
+                for coffeeItem in coffeeArray {
+                    guard let imageView = coffeeItem.imageView else { return stackView }
+                    guard let label = coffeeItem.label else { return stackView }
+                    stackViewItem.addArrangedSubview(imageView)
+                    stackViewItem.addArrangedSubview(label)
                 }
             }
             else {
-                for index in sizeOfCoffeArray/2...sizeOfCoffeArray - 1  {
-                    stackView.addArrangedSubview(self.coffeeArray[index].imageView!)
-                    stackView.addArrangedSubview(self.coffeeArray[index].label!)
+                for index in sizeOfCoffeArray / 2...sizeOfCoffeArray - 1 {
+                    guard let imageView = coffeArray[index].imageView else { return stackView }
+                    guard let label = coffeArray[index].label else { return stackView }
+                    stackViewItem.addArrangedSubview(imageView)
+                    stackViewItem.addArrangedSubview(label)
                 }
             }
-            
+
         }
+       
         return stackView
     }
-    
 
-    
-    private func initImageViewArray() -> [UIImageView] {
-        var imageViewArray = [UIImageView]()
-        
-        imageViewArray.append(UIImageView(image: #imageLiteral(resourceName: "cappuccino")))
-        imageViewArray.append(UIImageView(image: #imageLiteral(resourceName: "latte")))
-        imageViewArray.append(UIImageView(image: #imageLiteral(resourceName: "dark")))
-        imageViewArray.append(UIImageView(image: #imageLiteral(resourceName: "white")))
-        imageViewArray.append(UIImageView(image: #imageLiteral(resourceName: "mocha")))
-        imageViewArray.append(UIImageView(image: #imageLiteral(resourceName: "flatWhite")))
-        
-        
-        for image in imageViewArray {
-            image.contentMode = .scaleAspectFill
-            image.layer.masksToBounds = true
-            image.translatesAutoresizingMaskIntoConstraints = false
-            image.heightAnchor.constraint(equalToConstant: 116).isActive = true
-            image.widthAnchor.constraint(equalToConstant: 174).isActive = true
-        }
-        
-        return imageViewArray
-    }
-    
-    private func initImageTitleArray() -> [UILabel] {
-        var labelArray = [UILabel]()
-        
-        for _ in 1...self.imageViewArray.count {
-            labelArray.append(UILabel())
-        }
-        labelArray[0].text = "Cappuccino"
-        labelArray[1].text = "Latte"
-        labelArray[2].text = "Black Coffee"
-        labelArray[3].text = "White Coffee"
-        labelArray[4].text = "Mocha"
-        labelArray[5].text = "Flat White"
-        
-        
-        return labelArray
-    }
-    
-    
-    
     private func initFirstColumnStackView () -> UIStackView {
         let stackView = UIStackView()
         stackView.axis = NSLayoutConstraint.Axis.vertical
@@ -197,7 +173,7 @@ extension CafeDrinkMenuViewController {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }
-    
+
     private func initSecondColumnStackView () -> UIStackView {
         let stackView = UIStackView()
         stackView.axis = NSLayoutConstraint.Axis.vertical
@@ -210,7 +186,7 @@ extension CafeDrinkMenuViewController {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }
-    
+
     private func initImageStackView () -> UIStackView {
         let stackView = UIStackView()
         stackView.axis = NSLayoutConstraint.Axis.horizontal
@@ -222,7 +198,7 @@ extension CafeDrinkMenuViewController {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }
-    
+
 }
 
 extension CafeDrinkMenuViewController {
@@ -231,15 +207,23 @@ extension CafeDrinkMenuViewController {
             self.searchBar.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 32.0),
             self.searchBar.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
             self.searchBar.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16)
-        ])
+            ])
     }
     
+    private func activateTitleLabelConstraints() {
+        NSLayoutConstraint.activate([
+            self.titleLabel.topAnchor.constraint(equalTo: self.searchBar.bottomAnchor, constant: 16),
+            self.titleLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
+            ])
+    }
+    
+
     private func setImageStackViewConstraints() {
         NSLayoutConstraint.activate([
-            self.imageStackView.topAnchor.constraint(equalTo: self.searchBar.topAnchor, constant: 96),
+            self.imageStackView.topAnchor.constraint(equalTo: self.titleLabel.bottomAnchor, constant: 32),
             self.imageStackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
             self.imageStackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16)
-        ])
+            ])
     }
-    
+
 }
